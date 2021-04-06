2405199589e1c94b423e1f19dbf488641418a46f
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-04-05 23:41:46 -0600

    Moving Footer and Header folders to Components folder

--- src/resources/containers/index.js
+++ src/resources/containers/index.js
@@ -1,7 +1,7 @@
-export { default as Header } from './Header';
+export { default as Header } from '../components/Header';
 export { default as Hero } from './Hero';
 export { default as About } from './About';
 export { default as Courses } from './Courses';
 export { default as Podcast } from './Podcast';
-export { default as Footer } from './Footer';
+export { default as Footer } from '../components/Footer';
 export { default as Contact } from './Contact';
